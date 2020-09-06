5f9429e2e9e08fa2f420af12c726b6bc0b9b28d5
RasmusB <RasmusB@users.noreply.github.com>
2020-09-06 13:41:51 +0200

    Blinking with correct pin...

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ void delay(uint64_t duration) {
 
 static void gpio_setup() {
     // Our test LED is connected to Port A pin 11, so let's set it as output
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 }
 
 int main() {
